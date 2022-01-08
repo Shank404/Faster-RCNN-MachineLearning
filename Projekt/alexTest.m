6ae9be993bae65a6a6bc8603d482d1323bd1e06e
Julian Müller <julian.mueller77@web.de>
2022-01-08 15:02:21 +0100

    alexnet for step2?

---
+++ Projekt/alexTest.m
@@ -0,0 +1,30 @@
+% Test CNN Netz
+close all
+clear
+
+% Variablen zur Verteilung der Daten
+amountTrain = 0.5;                                  %Anzahl der Trainingsdaten
+amountVal = 0.1;                                    %Anzahl der Validierungsdaten
+amountTest = 0.4;                                   %Anzahl der Testdaten
+
+% Einlesen der erkannten Schilder
+imageDS = imageDatastore('SignsCutted','IncludeSubfolders',true,'LabelSource','foldernames');  % create DataStore
+
+% 50% der Bilder zum Trainieren, 10% zum Validieren, 40% zum Testen
+[trainingImageDS, validationImageDS, testImageDS] = splitEachLabel(imageDS, amountTrain, amountVal, amountTest,'randomized');
+
+% laden des vortrainierten Netzes
+load netAlexClassification.mat netTransfer;
+
+predictedLabels = classify(netTransfer, validationImageDS);
+accuracy = mean(predictedLabels == validationImageDS.Labels);
+
+% ein Bild entnehmen
+[T, info] = read(testImageDS);
+str = cellstr(info.Label);
+image(T);
+classify(netTransfer, T);
+
+% ----- Anwendung des trainierten Netzwerkes: Gegen den Gesamtdatensatz testen                          
+predictedLabels = classify(netTransfer, testImageDS);
+accuracy = mean(predictedLabels == testImageDS.Labels);

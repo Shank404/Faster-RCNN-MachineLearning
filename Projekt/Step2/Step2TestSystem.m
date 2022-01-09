0e3c262bec5ac5f6578a8bc74aaa6574432c264c
Fabian Beckdorf <fabian.beckdorf@stud.hs-flensburg.de>
2022-01-09 15:21:49 +0100

    First complete Step2TestSystem.m

--- Projekt/Step2/Step2TestSystem.m
+++ Projekt/Step2/Step2TestSystem.m
@@ -1,4 +1,32 @@
+clear;
+close all;
 
-run Step2TestRegionDetection.m
+addpath Step2;
+% --- Trainieren des Region Detection Netzwerks (zum Finden der Schilder im
+% Bild)
+Step2TrainRegionDetection
+%pause(3)    % Puffer zum Speichern der Datei
 
-run Step2TestClassification.m
+% --- Testen des Region Detection Netzwerks (zum Finden der Schilder im
+% Bild)
+if exist('Neuronale Netze/netDetectorResNet50.mat','file')
+   Step2TestRegionDetection
+else
+    disp('Error: Das Neuronale Netz ''netDetectorResNet50.mat'' wurde nicht gefunden');
+    return
+end
+
+% --- Trainieren des Classification Netzwerks (zum Identifizieren der 
+% gefundenen Schilder)
+Step2TrainClassification
+pause(3)    % Puffer zum Speichern der Datei
+
+run('Funktionen\resizeImages.m')
+% --- Testen des Classification Netzwerks (zum Finden der Schilder im
+% Bild)
+if (exist('SignsFound','dir') && exist('Neuronale Netze/netClassification.mat','file'))
+   Step2TestClassification
+else
+    disp('Error: Das Neuronale Netz ''netClassification.mat'' wurde nicht gefunden');
+    return
+end

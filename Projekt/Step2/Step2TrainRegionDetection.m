1c30797b050bbc5abab10fa0cca5e68b38bc7524
Nico.Isheim <75957170+Niskill2@users.noreply.github.com>
2022-01-09 14:52:57 +0100

    Kommentare angepasst & Struktur

--- Projekt/Step2/Step2TrainRegionDetection.m
+++ Projekt/Step2/Step2TrainRegionDetection.m
@@ -1,44 +1,49 @@
+% Gruppe 2
+% Fabian Beckdorf - 690047
+% Jacob Prütz - 690043
+% Ali Reza Teimoury - 690065
+% Julian Müller - 690018
+% Michael Sievers - 690593
+% Nico Isheim - 690222
+%------------------------------------------------------------------------%
+% Mithilfe dieses Skriptes wird das ResNet50 belernt.
+% Hierbei nutzen wir den uns zur Verfügung gestellten Datensatz, der
+% Straßenverkehrsschilder. Im Anschluss kann ein besipielhaft belernter
+% Datenpunkt ausgegeben werden.
+%------------------------------------------------------------------------%
+
 close all
 clear
 
+% ----- Zeige beispielhaftes Bild mit BoundingBox nach Training ----- %
+showExample = false;
 
+% ----- Definition inputSize & Rückgabe der Datastores ----- %
+inputSizeVec = [448 448 3]; %[768 1024 3];% inputSize definieren, mit der die Bilder eingelesen werden notwendig um Speicher der Grafikkarte nicht zu überladen
+[trainingDataDS,validationDataDS, testDataDS, testDataTbl, signDatasetTbl] = LoadAndRandomizeData(inputSizeVec);
 
-% ----- Ausgabe eines Bildes mit LabelBox ----- %
-% inputSize definieren, mit der die Bilder eingelesen werden
-% notwendig um Speicher der Grafikkarte nicht zu überladen
-inputSize = [448 448 3]; %[768 1024 3];
-[trainingDataDS,validationDataDS, testDataDS, testDataTbl, signDatasetTbl] = LoadAndRandomizeData(inputSize);
 % ----- Augmentierung der Daten ----- %
 augmentedTrainingData = transform(trainingDataDS,@augmentData); %u.a. horizontales spiegeln (siehe Funkton unten). Das ist vielleicht keine gute Idee !!
-trainingDataDS = transform(augmentedTrainingData,@(data)preprocessData(data,inputSize));
-validationDataDS = transform(validationDataDS,@(data)preprocessData(data,inputSize));
+trainingDataDS = transform(augmentedTrainingData,@(data)preprocessData(data,inputSizeVec));
+validationDataDS = transform(validationDataDS,@(data)preprocessData(data,inputSizeVec));
 
 % ----- Erstellung des Faster Regionbased Convolutional Neuronal Network ----- %
-preprocessedTrainingData = transform(trainingDataDS, @(data)preprocessData(data,inputSize));
-% Bilder werden auf die input Size skalliert
+preprocessedTrainingDataDS = transform(trainingDataDS, @(data)preprocessData(data,inputSizeVec)); % Bilder werden auf die input Size skalliert
 
 % ----- Ermittlung der Anchor-boxes ----- %
 numAnchors = 3;
-anchorBoxes = estimateAnchorBoxes(preprocessedTrainingData,numAnchors);
+anchorBoxesVec = estimateAnchorBoxes(preprocessedTrainingDataDS,numAnchors);
 
 % ----- Feature CNN definieren ----- %
 featureExtractionNetwork = resnet50;
 featureLayer = 'activation_40_relu';
 numClasses = width(signDatasetTbl)-1;    % Kategorien, die erkannt werden sollen ( hier 1: Schilder )
+lgraph = fasterRCNNLayers(inputSizeVec,numClasses,anchorBoxesVec,featureExtractionNetwork,featureLayer);
 
-lgraph = fasterRCNNLayers(inputSize,numClasses,anchorBoxes,featureExtractionNetwork,featureLayer);
+% ----- Netzwerk ansehen ----- %
+% analyzeNetwork(lgraph)
 
-layersTransfer = featureExtractionNetwork.Layers(1:end-3);
-layers = [
-    layersTransfer
-    fullyConnectedLayer(numClasses,'WeightLearnRateFactor',20,...
-        'BiasLearnRateFactor',20)
-    softmaxLayer
-    classificationLayer];
-
-
-% analyzeNetwork(lgraph) % ----- Netzwerk ansehen ----- %
-   
+% ----- Trainingsoptionen ----- %
 options = trainingOptions('sgdm',...
     'MaxEpochs',13,...
     'MiniBatchSize',1,...
@@ -47,20 +52,19 @@ options = trainingOptions('sgdm',...
     'ValidationData',validationDataDS,...
     'Plots','training-progress');
 
-% Train the Faster R-CNN detector.
-[detector, info] = trainFasterRCNNObjectDetector(trainingDataDS,lgraph,options); %, ...
-    %'NegativeOverlapRange',[0 0.3], ...
-    %'PositiveOverlapRange',[0.6 1]);
-% Trainiertes Netz abspeichern, um es in anderen Skripten verwenden zu
-% können
+% ----- Trainieren des Netzes und anschließendes sichern ----- %
+[detector, info] = trainFasterRCNNObjectDetector(trainingDataDS,lgraph,options);
 save netDetectorResNet50.mat detector;
 
-% ----- quick check/test ----- %
+% ----- Ausgabe eines beispielhaften Datenpunktes inkl. der BoundingBox ----- %
 if showExample
+    
+    % ----- Ermittlung eines zufälligen Bildes aus dem Datensatz ----- %
     showIndx = floor(rand()*length(testDataTbl.imageFilename)) % Für zufälliges Bild
     Img = imread(testDataTbl.imageFilename{showIndx});   %I = imread(testDataTbl.imageFilename{3});
     [bboxes,scores] = detect(detector,Img);
-    % ----- Ausgabe der Ergebnisse ----- %
+    
+    % ----- Ausgabe der Daten ----- %
     Img = insertObjectAnnotation(Img,'rectangle',bboxes,scores);
     figure
     imshow(Img)

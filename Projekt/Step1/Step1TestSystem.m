b50c0f0d77bd07f96f1c03b0b1e7b52e09a9c866
Fabian Beckdorf <fabian.beckdorf@stud.hs-flensburg.de>
2022-01-09 14:28:04 +0100

    Added if clauses to Step1TestSystem.m

--- Projekt/Step1/Step1TestSystem.m
+++ Projekt/Step1/Step1TestSystem.m
@@ -1,4 +1,31 @@
+clear;
+close all;
 
-run Step1TestRegionDetection.m
+% --- Trainieren des Region Detection Netzwerks (zum Finden der Schilder im
+% Bild)
+Step1TrainRegionDetection
+%pause(3)    % Puffer zum Speichern der Datei
 
-run Step1TestClassification.m
+% --- Testen des Region Detection Netzwerks (zum Finden der Schilder im
+% Bild)
+if exist('Neuronale Netze/netDetectorResNet50.mat','file')
+   Step1TestRegionDetection
+else
+    disp('Error: Das Neuronale Netz ''netDetectorResNet50.mat'' wurde nicht gefunden');
+    return
+end
+
+% --- Trainieren des Classification Netzwerks (zum Identifizieren der 
+% gefundenen Schilder)
+Step1TrainClassification
+pause(3)    % Puffer zum Speichern der Datei
+
+run('Funktionen\resizeImages.m')
+% --- Testen des Classification Netzwerks (zum Finden der Schilder im
+% Bild)
+if (exist('SignsFound','dir') && exist('Neuronale Netze/netClassification.mat','file'))
+   Step1TestClassification
+else
+    disp('Error: Das Neuronale Netz ''netClassification.mat'' wurde nicht gefunden');
+    return
+end
